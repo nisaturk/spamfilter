1dbca922549c77d38b37394b53afe32b8fe9812c
nisanoire <nisanoire@outlook.com>
2024-10-31 19:36:12 +0300

    Updated the model with n-gram for better filtration + changed the test/train ratio. Added some personal spam/ham texts I received to expand the dataset.

--- spamdetector.py
+++ spamdetector.py
@@ -1,12 +1,9 @@
-# Simple spam detection model for NLP course
-
 import pandas as pd
 import string
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
-
 
 file_path = 'spamorham.txt'
 
@@ -41,12 +38,11 @@ def preprocess_text(text):
     text = text.translate(str.maketrans('', '', string.punctuation))
     return text
 
-data['cleaned_message'] = data['message'].apply(preprocess_text) # Cleaning
+data['cleaned_message'] = data['message'].apply(preprocess_text)  # Cleaning
 
-print(data.head()) # Displaying a few
-
-# Creating a "Bag of Words" model
-vectorizer = CountVectorizer()
+# Creating a "Bag of Words" model with n-grams
+# Using n-grams for a better detection filter
+vectorizer = CountVectorizer(ngram_range=(1, 2), stop_words='english')  # Unigrams and bigrams
 X = vectorizer.fit_transform(data['cleaned_message'])  # Converting the messages to feature vectors
 y = data['label']  # Labels (ham or spam)
 
@@ -55,7 +51,7 @@ X_array = X.toarray()
 print("Feature vectors shape:", X_array.shape)
 
 # Splitting the dataset into sets of training and testing
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=82) # Now using a 70-30 split
 
 # Creating & training the model
 model = MultinomialNB()
@@ -75,12 +71,9 @@ def predict_message(message):
     prediction = model.predict(features)
     return prediction[0]
 
-#Testing the model manually, with some example texts:
-
-#new_message = "OMG! Did you just win the lottery? Your girlfriend is SO pretty!!"
-#new_message = "Hey its me, the guy from biology101."
-
-# For this bait text message, the model makes a wrong prediction. Further improvements should be made.
-new_message = "Congratulations! You have been selected for a free trial of our amazing weight loss product. Sign up today to start your journey!"
-
+# Testing the model manually, with some example texts:
+    
+#new_message = "Hey its me, the guy from biology101." #ham
+#new_message = "Congratulations! You have been selected for a free trial of our amazing weight loss product. Sign up today to start your journey!" #spam
+new_message = "Hey Isabel! Long time no see, we missed you! We put a 20 USD worth of coupon for you to use on our halloween collection! Offer ends on 31 October" #ham
 print(f"The message is classified as: {predict_message(new_message)}")
